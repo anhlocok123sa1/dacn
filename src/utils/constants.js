641f6a80c04e20cea30ed45fa7bca8a5246aa80c
anhlocok123sa1 <100480904+anhlocok123sa1@users.noreply.github.com>
2023-12-25 12:32:54 +0700

    commit

---
+++ src/utils/constants.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { GiCompass, GiDiamondHard, GiStabbedNote } from 'react-icons/gi'
+export const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'products',
+    url: '/products',
+  },
+]
+
+export const services = [
+  {
+    id: 1,
+    icon: <GiCompass />,
+    title: 'mission',
+    text:
+      'Thiết kế cho bạn',
+  },
+  {
+    id: 2,
+    icon: <GiDiamondHard />,
+    title: 'vision',
+    text:
+      'Thiết kế cho bạn',
+  },
+  {
+    id: 3,
+    icon: <GiStabbedNote />,
+    title: 'history',
+    text:
+      'Thiết kế cho bạn',
+  },
+]
+
+export const products_url = 'https://course-api.com/react-store-products'
+
+export const single_product_url = `https://course-api.com/react-store-single-product?id=`
